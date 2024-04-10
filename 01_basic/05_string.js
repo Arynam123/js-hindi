745c966327fe7ffb539bcbea090d25d599359149
Arynam123 <141307890+Arynam123@users.noreply.github.com>
2024-04-10 17:34:28 +0000

    commit chang e

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -12,4 +12,32 @@ console.log(`Hello my name is ${name} and my repocount is ${repocount}`)
 
 //playing with stirng
 const gameName=  new String('aryan-hc-com')
-console.log(gme)
+console.log(gameName.charAt(8));
+console.log(gameName.indexOf('h'));
+
+
+const newstring=gameName.slice(-9,-1);
+const newstring1=gameName.substring(2,7);
+
+
+console.log(newstring);
+console.log(newstring1);
+
+const trimstirng="   aryan  kumar   ";
+console.log(trimstirng.trimStart());
+console.log(trimstirng.trimEnd());
+console.log(trimstirng.trim());
+
+
+
+
+const url="https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20','-'));
+console.log(url.includes('aryan'));//return true or false
+
+//change to an array
+console.log(url.split('/'));
+
+
+//i have to practise more 10-15 string functions
+
