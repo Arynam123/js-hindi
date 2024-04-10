256dd22c9297372db8e124951c85ed2aee7b410a
Arynam123 <141307890+Arynam123@users.noreply.github.com>
2024-04-10 19:30:39 +0000

    fc

---
+++ 01_basic/07_date.js
@@ -0,0 +1,23 @@
+let mydate=new Date();
+console.log(typeof mydate);
+console.log(mydate)
+
+console.log(mydate.toString());//date+time+gmt
+
+console.log(mydate.toDateString())//only date
+
+console.log(mydate.toISOString());
+
+console.log(mydate.toJSON())
+console.log(mydate.toLocaleDateString('en-IN'))//converted to local format
+
+console.log(mydate.toLocaleTimeString());
+
+console.log(mydate.toLocaleTimeString('en-IN'));
+
+console.log(mydate.toTimeString()) //gmt time
+console.log(mydate.toUTCString())//gmt time
+
+
+
+
