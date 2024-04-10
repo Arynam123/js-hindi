102c07fac4e8dc8f6778fc7c10189869eda28b5f
Arynam123 <141307890+Arynam123@users.noreply.github.com>
2024-04-10 10:56:26 +0000

    restartg

---
+++ 01_basic/05_string.js
@@ -0,0 +1,15 @@
+const name="NagaArjun";
+const repocount=10;
+
+
+//old version of printing
+console.log(name+" my repocount is "+repocount);
+
+
+//new version of printing in javascript
+console.log(`Hello my name is ${name} and my repocount is ${repocount}`)
+
+
+//playing with stirng
+const gameName=  new String('aryan-hc-com')
+console.log(gme)
