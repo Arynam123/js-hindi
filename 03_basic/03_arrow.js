9e2408359efafa1c1045c98010c5cd8a49f99e02
Arynam123 <141307890+Arynam123@users.noreply.github.com>
2024-04-13 22:17:39 +0000

    aa

---
+++ 03_basic/03_arrow.js
@@ -0,0 +1,12 @@
+function chai(){
+        let username = "hitesh"
+         console.log(this.username);
+     }
+    
+chai()
+let chaiaurcode=function(){
+    let username="aryan"
+    console.log(this.username)
+}
+chaiaurcode()
+    
