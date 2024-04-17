9a5695a8dd5c8e84f333defc463d6682051a0b70
Arynam123 <141307890+Arynam123@users.noreply.github.com>
2024-04-17 12:20:58 +0000

    while

---
+++ 05_iteration.js/while.js
@@ -0,0 +1,23 @@
+let i=0;
+while (i<10){
+    console.log("value of i is",i)  
+    i++;
+}
+
+
+let myarr=["Aryan","Vinod","Kapse","Raman","Batra"]
+let j=0;
+while(myarr.length>j){
+    console.log(myarr[j])
+    j++;
+}
+
+//playing with do while
+
+// do while ek baar toh run hona hi hona hai
+
+i=4
+do {
+    console.log("Vison is to learn web devlopment")
+    
+} while (i>5);
